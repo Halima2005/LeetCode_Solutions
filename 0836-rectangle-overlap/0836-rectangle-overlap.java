37b57c0e230ed750549df4e6a2f6975743de46a4
Halima2005 <150766132+Halima2005@users.noreply.github.com>
2026-09-14 10:47:25 +0530

    Time: 0 ms (100%), Space: 42.6 MB (52.92%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        boolean xOverlap = rec1[2] >rec2[0] && rec2[2]>rec1[0];
+        boolean yOverlap = rec1[3]>rec2[1] && rec2[3]>rec1[1];
+
+        return xOverlap && yOverlap;
+
+    }
+}
